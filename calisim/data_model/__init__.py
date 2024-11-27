ef96a8f0e6e516c099203fafff93578b9a561b2c
James Bristow <jbris@users.noreply.github.com>
2024-11-28 12:34:31 +1300

    Exporting ParameterModel data model

--- calisim/data_model/__init__.py
+++ calisim/data_model/__init__.py
@@ -2,10 +2,12 @@ from .simulation_data_models import (
 	CalibrationModel,
 	DistributionModel,
 	ParameterDataType,
+	ParameterModel,
 	ParameterSpecification,
 )
 
 __all__ = [
+	ParameterModel,
 	DistributionModel,
 	ParameterDataType,
 	CalibrationModel,
