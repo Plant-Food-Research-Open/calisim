3c0b127e2cd579889c21178cb24fd1a72c9825e6
James Bristow <james.bristow@plantandfood.co.nz>
2025-04-26 23:44:43 +1200

    Increasing number of samples for reliability functional tests

--- tests/functional/test_reliability.py
+++ tests/functional/test_reliability.py
@@ -26,7 +26,7 @@ def test_openturns_monte_carlo(
 
 	calibration_kwargs = dict(
 		method="monte_carlo",
-		n_samples=100,
+		n_samples=300,
 		comparison="less_or_equal",
 		threshold=20,
 		n_jobs=1,
@@ -59,7 +59,40 @@ def test_openturns_subset(
 
 	calibration_kwargs = dict(
 		method="subset",
-		n_samples=100,
+		n_samples=300,
+		comparison="less_or_equal",
+		threshold=20,
+		n_jobs=1,
+		calibration_func_kwargs=dict(t=observed_data.day),
+	)
+
+	calibrator = get_calibrator(
+		ReliabilityAnalysisMethod,
+		ReliabilityAnalysisMethodModel,
+		sir_model,
+		sir_parameter_spec,
+		"openturns",
+		outdir,
+		sir_model.output_labels,
+		calibration_kwargs,
+		l2_norm_metric,
+	)
+
+	calibrator.specify().execute().analyze()
+	assert calibrator.implementation.sampler is not None
+
+
+def test_openturns_sobol(
+	sir_model: ExampleModelContainer,
+	sir_parameter_spec: ParameterSpecification,
+	outdir: str,
+	l2_norm_metric: DistanceMetricBase,
+) -> None:
+	observed_data = sir_model.observed_data
+
+	calibration_kwargs = dict(
+		method="sobol",
+		n_samples=300,
 		comparison="less_or_equal",
 		threshold=20,
 		n_jobs=1,
